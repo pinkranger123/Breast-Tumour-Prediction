eef7a8df3279b6d0d4665b2e95ab7df94df2449e
pinkranger123 <123815909+pinkranger123@users.noreply.github.com>
2023-11-19 12:34:58 +0530

    Update tumour-predict-logistic-regression.py

--- tumour-predict-logistic-regression.py
+++ tumour-predict-logistic-regression.py
@@ -7,7 +7,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
 # Load the dataset
-url = 'your_dataset_file_path.csv'  # Replace with the actual path to your CSV file
+url = 'breast_cancer.csv'  # Replace with the actual path to our CSV file
 data = pd.read_csv(url)
 
 # Preprocess the data
